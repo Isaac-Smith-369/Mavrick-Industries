9502dbea2e8dc088ba1d88cd92c671b530f8fd00
Isaac K.B Smith <76779285+Isaac-Smith-369@users.noreply.github.com>
2023-03-01 11:38:00 +0000

    Add files via upload
    rewrite

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
+
+document.querySelector(".btn").addEventListener("click", function () {
+  alert("Service is not available at the moment.");
+});
